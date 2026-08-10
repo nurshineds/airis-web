f895c6b23f4950a37e611884e87c9b53e2519743
iGipxy <gesangrah@gmail.com>
2026-08-10 07:32:16 +0700

    Add MonitoringHistory component and styles; update routing for IndikatorPengukuran page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import KondisiUdara from "./pages/KondisiUdara/KondisiUdara";
+import IndikatorPengukuran from "./pages/IndikatorPengukuran/IndikatorPengukuran";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/KondisiUdara" element={<KondisiUdara />} />
+            <Route path="/IndikatorPengukuran" element={<IndikatorPengukuran />} />
         </Routes>
     );
 }
---
+++ frontend/src/pages/IndikatorPengukuran/IndikatorPengukuran.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import ISPUInfo from "../../components/ISPUInfo/ISPUInfo";
+
+export default function IndikatorPengukuran() {
+    return (
+        <>
+            <Navbar />
+            <main>
+                <ISPUInfo />
+            </main>
+            <Footer />
+        </>
+    );
+}
