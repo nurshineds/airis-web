ea423206792e3c2f85c320868e7554d17aa3f7d4
kasurempukamba <kasurempukamba@gmail.com>
2026-08-11 20:25:42 +0700

    .
    .

--- backend/src/routes/contentRoute.js
+++ backend/src/routes/contentRoute.js
@@ -7,6 +7,7 @@ const contentController = require("../controllers/contentController");
 //ini bener gini ga sih yang authAdmin? ^_____^
 const { authAdmin } = require("../middleware/authMiddleware");
 
+router.get("/get-content/:idContent", contentController.getContentbyID);
 router.post("/add-content", authAdmin, contentController.createContent);
 router.post("/update-content", authAdmin, contentController.updateContent);
 router.post("/delete-content", authAdmin, contentController.deleteContent);
